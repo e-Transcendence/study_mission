1fcef43ba053121d221fef168853f35560dea550
e-Transcendence-Brainiac <461004827@qq.com>
2020-10-30 20:55:45 +0800

    1

--- part_3_BagOfCentroids.py
+++ part_3_BagOfCentroids.py
@@ -4,62 +4,10 @@ from gensim.models import Word2Vec
 import numpy as np
 import pandas as pd
 from study_mission.part_2_word2vec import review_to_wordlist, num_features
-from sklearn.ensemble import RandomForestClassifier
 
 model = Word2Vec.load("D:\All_project\study_mission\\300features_40minwords_10context")
 train = pd.read_csv("D:\All_project\study_mission\\labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
 test = pd.read_csv("D:\All_project\study_mission\\testData.tsv", header=0, delimiter="\t", quoting=3)
-
-
-def makeFeatureVec(words, model, num_features):
-    # Function to average all of the word vectors in a given
-    # paragraph
-    #
-    # Pre-initialize an empty numpy array (for speed)
-    featureVec = np.zeros((num_features,), dtype="float32")
-    #
-    nwords = 0
-    #
-    # Index2word is a list that contains the names of the words in
-    # the model's vocabulary. Convert it to a set, for speed
-    index2word_set = set(model.wv.index2word)
-    #
-    # Loop over each word in the review and, if it is in the model's
-    # vocaublary, add its feature vector to the total
-    for word in words:
-        if word in index2word_set:
-            nwords = nwords + 1
-            featureVec = np.add(featureVec, model[word])
-    #
-    # Divide the result by the number of words to get the average
-    featureVec = np.divide(featureVec, nwords)
-    return featureVec
-
-
-def getAvgFeatureVecs(reviews, model, num_features):
-    # Given a set of reviews (each one a list of words), calculate
-    # the average feature vector for each one and return a 2D numpy array
-    #
-    # Initialize a counter
-    counter = 0
-    #
-    # Preallocate a 2D numpy array, for speed
-    reviewFeatureVecs = np.zeros((len(reviews), num_features), dtype="float32")
-
-    # Loop through the reviews
-    for review in reviews:
-        # Print a status message every 1000th review
-        if counter % 1000 == 0:
-            print("Review %d of %d" % (counter, len(reviews)))
-        # Call the function (defined above) that makes average feature vectors
-        reviewFeatureVecs[counter] = makeFeatureVec(review, model, num_features)
-        # Increment the counter
-        counter = counter + 1
-    return reviewFeatureVecs
-
-
-
-
 
 # Set "k" (num_clusters) to be 1/5th of the vocabulary size, or an
 # average of 5 words per cluster
@@ -69,12 +17,10 @@ num_clusters = int(word_vectors.shape[0] / 5)
 kmeans_clustering = KMeans(n_clusters=num_clusters)
 idx = kmeans_clustering.fit_predict(word_vectors)
 
-
 # Create a Word / Index dictionary, mapping each vocabulary word to
 # a cluster number
 word_centroid_map = dict(zip(model.wv.index2word, idx))
 
-"""
 # For the first 10 clusters
 for cluster in range(0, 10):
     #
@@ -142,4 +88,3 @@ result = forest.predict(test_centroids)
 # Write the test results
 output = pd.DataFrame(data={"id": test["id"], "sentiment": result})
 output.to_csv("BagOfCentroids.csv", index=False, quoting=3)
-"""
